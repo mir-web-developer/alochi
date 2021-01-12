e4e0a03a75d06d1b7c7c1bff425265c900089c0e
Mirjaxon <djalilovmir@gmail.com>
2021-01-13 01:53:43 +0300

    new-tests-added

--- src/reducers/germanTests7Reducer.js
+++ src/reducers/germanTests7Reducer.js
@@ -3,24 +3,168 @@ const initialState = {
   question7class: [
     {
       questionId: 0,
-      questionName: "II Viertel V KLASS I VARIANT",
+      questionName: "I Viertel VII KLASS I VARIANT",
       questions: [
         {
-          questionText: "Monika … 11 Jahre alt.",
+          questionText: "Ergänzen Sie! Im Süden grezt Deutschland an  … ",
           answerOptions: [
-            { answerText: "ist", isCorrect: true },
-            { answerText: "seid", isCorrect: false },
-            { answerText: "sind", isCorrect: false },
-            { answerText: "sein", isCorrect: false },
+            { answerText: "Polen", isCorrect: false },
+            { answerText: "die Schweiz", isCorrect: true },
+            { answerText: "Frankreich", isCorrect: false },
+            { answerText: "Belgien", isCorrect: false },
           ],
         },
         {
-          questionText: "Was ... du in der Tasche?",
+          questionText: "Nordlich von Deutschland liegt ... .",
           answerOptions: [
-            { answerText: "ist", isCorrect: false },
-            { answerText: "bist", isCorrect: true },
-            { answerText: "bin", isCorrect: false },
-            { answerText: "sein", isCorrect: false },
+            { answerText: "die Schweiz", isCorrect: false },
+            { answerText: "Dänemark", isCorrect: true },
+            { answerText: "Polen", isCorrect: false },
+            { answerText: "die Niderlande", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Wie heisst die Hauptstadt von Nordrhein Westfahlen?",
+          answerOptions: [
+            { answerText: "Hamburg", isCorrect: false },
+            { answerText: "Düsseldorf", isCorrect: true },
+            { answerText: "Bonn", isCorrect: false },
+            { answerText: "Stuttgart", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Wo befindet sich Bethovenshaus?",
+          answerOptions: [
+            { answerText: "in Köln", isCorrect: false },
+            { answerText: "in Bonn", isCorrect: true },
+            { answerText: "in Berlin", isCorrect: false },
+            { answerText: "in Hamburg", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Setzen Sie passende  Konjunktion ein. Ich weiss , …. er   wirklich kommt.",
+          answerOptions: [
+            { answerText: "weil", isCorrect: false },
+            { answerText: "dass", isCorrect: true },
+            { answerText: "den", isCorrect: false },
+            { answerText: "ob", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Setzen Sie passende  Konjunktion ein. Ich freue mich,  ..….  die Sonne scheint. ",
+          answerOptions: [
+            { answerText: "dass", isCorrect: true },
+            { answerText: "ob", isCorrect: false },
+            { answerText: "weil", isCorrect: false },
+            { answerText: "deshalb", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Ergänzen Sie! In der Stadt gibt es   die  ……. Diskos.",
+          answerOptions: [
+            { answerText: "beste", isCorrect: false },
+            { answerText: "bestes", isCorrect: false },
+            { answerText: "besten", isCorrect: true },
+            { answerText: "bester", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Setzen Sie  passende Präpostion ein.Ich möchte   ..….  meinem Dorf erzählen.",
+          answerOptions: [
+            { answerText: "von", isCorrect: true },
+            { answerText: "über", isCorrect: false },
+            { answerText: "in", isCorrect: false },
+            { answerText: "aus", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Ergänzen Sie folgenden Satz. Ich brauche viel Geld, ...",
+          answerOptions: [
+            { answerText: "um eine Wohnung zu kaufen", isCorrect: true },
+            { answerText: "um gut zu lernen", isCorrect: false },
+            { answerText: "um  gut Fuβball zu spielen", isCorrect: false },
+            { answerText: "um  schnell zu laufen", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Ich lerne Deutsch, ... .",
+          answerOptions: [
+            { answerText: "um gut zu leben", isCorrect: false },
+            { answerText: "um ein Auto zu kaufen", isCorrect: false },
+            { answerText: "um in Deutschland zu studieren.", isCorrect: true },
+            { answerText: "um viel Geld verdienen", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Was brauchst du um Tennis zu spielen?",
+          answerOptions: [
+            { answerText: "einen Tisch", isCorrect: false },
+            { answerText: "einen Schläger", isCorrect: true },
+            { answerText: "ein Korb", isCorrect: false },
+            { answerText: "ein Papier", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Ergänzen Sie folgenden Satz. Ich habe  ….. verbrannt.",
+          answerOptions: [
+            { answerText: "mir", isCorrect: false },
+            { answerText: "dich", isCorrect: false },
+            { answerText: "mich", isCorrect: true },
+            { answerText: "sich", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Wenn du gut Fussball spielen willst,... du schnell  laufen.",
+          answerOptions: [
+            { answerText: "muss", isCorrect: false },
+            { answerText: "darfst", isCorrect: false },
+            { answerText: "kannst", isCorrect: true },
+            { answerText: "willst", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Ich habe ... den Kopf gestoβen.",
+          answerOptions: [
+            { answerText: "mich", isCorrect: false },
+            { answerText: "dich", isCorrect: false },
+            { answerText: "dir", isCorrect: false },
+            { answerText: "mir", isCorrect: true },
+          ],
+        },
+        {
+          questionText: "Ergänzen Sie folgenden Satz. Wenn es schneit,",
+          answerOptions: [
+            { answerText: "trage ich einen Mantel.", isCorrect: true },
+            { answerText: "trage ich Brille.", isCorrect: false },
+            { answerText: "trage ich eine Jacke.", isCorrect: false },
+            { answerText: "trage ich ein Kleid.", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Wenn es regnet, ... .",
+          answerOptions: [
+            { answerText: "trage ich Brille.", isCorrect: false },
+            { answerText: "trage ich eine Jacke.", isCorrect: false },
+            { answerText: "trage ich ein Kleid.", isCorrect: false },
+            { answerText: "trage ich einen Regenmantel", isCorrect: true },
+          ],
+        },
+        {
+          questionText: "Dieses Jahr reise ich ……. Schweiz. ",
+          answerOptions: [
+            { answerText: "in der", isCorrect: true },
+            { answerText: "in das", isCorrect: false },
+            { answerText: "in die", isCorrect: false },
+            { answerText: "in den", isCorrect: false },
+          ],
+        },
+        {
+          questionText: "Setzen Sie passende Relativpronomen ein. Das Kind möchte den Ball spielen, …. im Garten liegt.",
+          answerOptions: [
+            { answerText: "das", isCorrect: false },
+            { answerText: "die", isCorrect: false },
+            { answerText: "den", isCorrect: false },
+            { answerText: "der", isCorrect: true },
           ],
         },
       ],
